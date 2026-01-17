2b31bbb227544915081c93e24e8aaed627a1a1d5
Moez <mmoez10@proton.me>
2026-01-17 22:41:33 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+var shortGolang = "Watch Go crash course"
+var fullGolang = "Watch Nana's Golang Full Course"
+var rewardDessert = "Reward myself with a donut"
+var taskItems = []string{shortGolang, fullGolang, rewardDessert}
+
+func main() {
+	fmt.Println("##### Welcome to our Todolist App! #####")
+	http.HandleFunc("/", helloUser)
+	http.HandleFunc("/show-tasks", showTasks)
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func showTasks(writer http.ResponseWriter, request *http.Request) {
+	for _, task := range taskItems {
+		fmt.Fprintln(writer, task)
+	}
+
+}
+
+func helloUser(writer http.ResponseWriter, request *http.Request) {
+	var greeting = "Hello user."
+	fmt.Fprintln(writer, greeting)
+}
+
+func printTasks(taskItems []string) {
+	fmt.Println("List of my Todos")
+	for index, task := range taskItems {
+		fmt.Printf("%d: %s\n", index+1, task)
+	}
+
+}
+
+func addTask(taskItems []string, newTask string) []string {
+	updatedTaskItems := append(taskItems, newTask)
+	printTasks(updatedTaskItems)
+	return updatedTaskItems
+}
